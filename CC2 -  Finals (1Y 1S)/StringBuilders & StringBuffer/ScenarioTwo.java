d859433522356f4e237c738ec67ae213ff3f11d4
Pao <kingpaolofranco60@gmail.com>
2023-11-20 10:06:15 +0800

    Scenario Two

---
+++ CC2 -  Finals (1Y 1S)/StringBuilders & StringBuffer/ScenarioTwo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * @author pao
+ * BSCS 1B
+ */
+public class ScenarioTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        System.out.println("--------------");
+        System.out.println("| Scenario 2 |");
+        System.out.println("--------------");
+        System.out.print("Enter first string  : ");
+        String str1 = sc.nextLine();
+        sb.append(str1);
+        System.out.print("Enter second string : ");
+        String str2 = sc.nextLine();
+        sb.append(str2);
+        System.out.println("Concatenated string: " + sb);
+        System.out.println("Concatenated length: " + sb.length());    }
+}
