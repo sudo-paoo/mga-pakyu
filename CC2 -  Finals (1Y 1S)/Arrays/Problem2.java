b05fc07fbca0b07fcf137185165f0f37a4a3a590
Pao <kingpaolofranco60@gmail.com>
2023-12-04 18:41:45 +0800

    Update Problem2.java

--- CC2 -  Finals (1Y 1S)/Arrays/Problem2.java
+++ CC2 -  Finals (1Y 1S)/Arrays/Problem2.java
@@ -6,12 +6,18 @@ import java.util.Arrays;
  */
 public class Problem2 {
     public static void main(String[] args) {
+        System.out.println("------------------");
+        System.out.println("| Array Rotation |");
+        System.out.println("------------------");
+        
         Scanner sc = new Scanner(System.in);
         int arrayNum[] = {1,2,3,4,5};
+        int n = arrayNum.length;
+        
         System.out.print("Enter rotation: ");
         int rotation = sc.nextInt();
+        
         System.out.println("Old array: " + Arrays.toString(arrayNum));
-        int n = arrayNum.length;
 
         for (int i = 0; i < rotation; i++) {
             int lastElement = arrayNum[n - 1];
