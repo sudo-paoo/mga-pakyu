04fb047892a1423409a3fa278f03799455d792e8
Pao <kingpaolofranco60@gmail.com>
2023-12-09 09:02:44 +0800

    rename

--- CC2 -  Finals (1Y 1S)/Lab Exam (Free)/Matrix.java
+++ CC2 -  Finals (1Y 1S)/Lab Exam (Free)/Matrix.java
@@ -3,7 +3,7 @@
  * BSCS 1B
  */
 import java.util.Scanner;
-public class Lab {
+public class Matrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("----------------------");
