6d7c6130a53aad34b492304300912ea372d1418f
Pao <kingpaolofranco60@gmail.com>
2024-02-29 09:41:36 +0800

    fix code format

--- CC3_ComProg2/CC3_Midterms/Quizzes/quiz_one.py
+++ CC3_ComProg2/CC3_Midterms/Quizzes/quiz_one.py
@@ -7,8 +7,7 @@ interest_amount = borrowed_amount * INTEREST
 total_loan = round(borrowed_amount + interest_amount, 2)
 daily_pay = round(total_loan / days_to_pay, 2)
 
-print(
-    f"""
+print(f"""
 ===================================
 | Name: {name}
 | Amount Borrowed: {borrowed_amount}
@@ -16,5 +15,4 @@ print(
 | Total Interest: {interest_amount}
 | Total Amount to be Paid: {total_loan}
 | Daily Pay: {daily_pay}
-==================================="""
-)
+===================================""")
