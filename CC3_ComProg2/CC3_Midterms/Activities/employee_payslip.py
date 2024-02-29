d4328d5c2bea9a2a006f72480e6291df9ad676dd
Pao <kingpaolofranco60@gmail.com>
2024-02-29 09:40:57 +0800

    Update and rename activity_one.py to employee_payslip.py

--- CC3_ComProg2/CC3_Midterms/Activities/employee_payslip.py
+++ CC3_ComProg2/CC3_Midterms/Activities/employee_payslip.py
@@ -9,20 +9,20 @@ sss = float(input("Enter SSS contribution: "))
 pag_ibig = float(input("Enter Pag-Ibig contribution: "))
 housing_loan = float(input("Enter housing loan amount: "))
 
-print()
-print("========== PAY SLIP ==========")
-
 salary = (hrs_work * RPH) + (hrs_ot * RPH_OT)
 salary_tax = salary * TAX
 total_deduction = salary_tax + sss + pag_ibig + housing_loan
 net_salary = salary - total_deduction
 
-print("NAME:", name)
-print("GROSS SALARY:", salary)
-print("SSS:", sss)
-print("Pag-Ibig:", pag_ibig)
-print("Housing Loan:", housing_loan)
-print("TAX:", salary_tax)
-print("TOTAL DEDUCTIONS:", total_deduction)
-print("NET SALARY:", net_salary)
-print("==============================")
+print(f"""
+|----------------------------|
+|          PAY SLIP          |
+|----------------------------|
+| Name: {name.upper()}
+| Gross Salary: {salary}
+| SSS: {sss}
+| Pag-Ibig: {pag_ibig}
+| Housing Loan: {housing_loan}
+| Tax: {salary_tax}
+| Total Deductions: {total_deduction}
+| Net Salary: {net_salary}""")
