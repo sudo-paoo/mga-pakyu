ff7db7c97504073166d1954cf581fd1ebcfb20c9
Pao <kingpaolofranco60@gmail.com>
2024-02-29 09:36:03 +0800

    Delete activity_two.py

--- CC3_ComProg2/CC3_Midterms/Activities/activity_two.py
+++
@@ -1,4 +0,0 @@
-sentence = input("Enter a sentence with REPEATED WORDS: ").lower()
-replaced = input("Enter the word TO BE REPLACED: ").lower()
-replacement = input("Enter the REPLACEMENT WORD: ").lower()
-print(sentence.replace(replaced, replacement).upper())
